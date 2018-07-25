3b34c1dd900f4e0b84d4bb98468ff68b01b6daa3
gradampl <gr_adam@o2.pl>
2018-07-25 15:30:30 +0200

    Service layer with draft ListRecipeService TEST

---
+++ DataLayer/RecipeTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xunit;
+using TestSupport.EfHelpers;
+using System.Linq;
+using FluentAssertions;
+using Aplikacja_kulinarna.Models;
+using Aplikacja_kulinarna.Data;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test
+{
+    public class RecipeTest
+    {
+       [Fact]
+         public void ShouldSaveRecipe()
+         {
+             //given
+             var options = SqliteInMemory
+                 .CreateOptions<CulinaryContext>();
+             using (var context = new CulinaryContext(options))
+             {
+                 //when
+                 context.Database.EnsureCreated();
+                 var r = new Recipe
+                 {
+                     Dish = "Cup of tea",
+                     DishRecipe = "Boil water and add tea",
+                     MinutesToPrepare = 5,
+                     QualityStars = 1,
+                 };
+                 context.Recipes.Add(r);
+                 context.SaveChanges();
+               
+             }
+ 
+             using (var context = new CulinaryContext(options))
+             {
+                 //VERIFY  
+                 var recipe = context.Recipes.First();
+                 recipe.Dish.Should().BeEquivalentTo("Cup of tea");
+                 recipe.DishRecipe.Should().BeEquivalentTo("Boil water and add tea");
+                 recipe.MinutesToPrepare.ShouldEqual(5);
+                 recipe.QualityStars.ShouldEqual(1);
+             }
+ 
+            
+         }
+    }
+}
---
+++ ServicesLayer/ListRecipeServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+using TestSupport.EfHelpers;
+using System.Linq;
+using FluentAssertions;
+using Aplikacja_kulinarna.Models;
+using Aplikacja_kulinarna.Data;
+using Xunit.Extensions.AssertExtensions;
+using Aplikacja_kulinarna.Services.RecipeServices.Concrete;
+
+namespace Test.ServicesLayer
+{
+    public class ListRecipeServiceTest
+    {
+       [Fact]
+         public void ShouldListRecipes()
+         {
+             //SETUP
+             var options = SqliteInMemory
+                 .CreateOptions<CulinaryContext>();
+             var numRecipes = 5;
+            
+             using (var context = new CulinaryContext(options))
+             {
+                 
+                 context.Database.EnsureCreated();
+
+                 for (var i = 0; i < numRecipes ; i++)
+                 {
+                      var r = new Recipe
+                     {
+                        Dish = $"Cup of tea #{i}",
+                        DishRecipe = $"Boil water and add tea for {i} minute(s) ",
+                        MinutesToPrepare = i,
+                        QualityStars = i,
+                     };
+
+                   context.Recipes.Add(r);
+                 }
+
+                 context.SaveChanges();
+
+                 //ATTEMPT
+                 var service = new ListRecipeService(context);
+                 var dtos = service.SelectAll().ToList();
+
+                 //VERIFY
+                 dtos.Count.ShouldEqual(numRecipes);
+             }
+     
+         }
+    }
+}
